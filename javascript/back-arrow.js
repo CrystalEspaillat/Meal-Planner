231e330fed67175f53b3919d123333839f046181
Crystal Espaillat <crystalespaillat@Crystals-MacBook-Pro.local>
2018-07-12 19:27:21 -0400

    Added some comments to javascript. Added margin to bottom of recipes content

--- javascript/back-arrow.js
+++ javascript/back-arrow.js
@@ -1,23 +1,31 @@
-// when the user clicks the meal icon ...
+// when the user clicks the back arrow ...
 $(".back-arrow").on("click", function() {
         //slide icon back to center
         $(".icon-div").addClass("animate close-panels");
         $(".icon-div").removeClass("animate slide-icon");
 
-
-        // hide the panels
+        // hide the recipe side panel
         $(".right-side").removeClass("display");
+        //hide the search div
         $(".search-div").removeClass("display");
+        //hide the menu div
         $(".menu-div").removeClass("display");
+        //hide the back arrow itself
         $(".back-arrow").removeClass("display");
 
         // show content to the right
         $(".content").fadeOut(1050);
 
+        // Show the meal icons
         $('.meal-icon').show();
+
+        // if the meal plan was clicked ....
         if($('.meal-icon').attr('click-status') === 'yes'){
+
+                // Toggle back to unclicked status
                 $(this).attr('click-status', 'no');
         }
 
+        // Clear out search form
         $('#search').val('');
 });
